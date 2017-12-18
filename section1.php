97fb16d05d0ddb05b586969491093381898232a5
Adam <adam@adamfly.me>
2017-12-17 21:53:40 -0500

    test

---
+++ section1.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Multi-User Input Demo</title>
+
+    <!-- CSS assets -->
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+
+    <!--[if lt IE 9]>
+
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+
+    <!-- JS assets -->
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+
+
+
+
+  </head>
+
+  <body>
+
+
+
+<!-- Navbar -->
+
+<div class="container">
+
+<div class="jumbotron">
+
+    <div id="center">
+
+        <h2>Interact District Conference 2018 Registration</h2>
+
+        <h4>Please select if you are a group or an individual:</h4>
+
+        <br>
+
+        <p><button type="button" class="btn btn-primary" onclick="location.href = 'section6.php'"><i class="fas fa-users fa-lg"></i>&nbsp;&nbsp;Group</button></p>
+
+        <p><button type="button" class="btn btn-primary" onclick="location.href = 'section2.php'"><i class="fas fa-user fa-lg"></i>&nbsp;&nbsp;Individual</button></p>
+
+    </div>
+
+</div>
+
+</div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
